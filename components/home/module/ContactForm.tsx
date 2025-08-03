19cfb5ece5840b0e2c8de7fc1cc9b81fa23038b9
UNAINR <munain605@gmail.com>
2025-08-03 09:41:36 +0500

    first commit

--- components/home/module/ContactForm.tsx
+++ components/home/module/ContactForm.tsx
@@ -102,8 +102,8 @@ export default function ContactForm() {
 							transition={{ duration: 0.5, delay: 0.5 }}
 							className="relative my-8 flex items-center justify-center overflow-hidden pr-8">
 							<div className="flex flex-col items-center justify-center overflow-hidden">
-								<article className="relative mx-auto h-[350px] min-h-60 max-w-[450px] overflow-hidden rounded-3xl border bg-gradient-to-b from-[#e60a64] to-[#e60a64]/5 p-6 text-3xl tracking-tight text-white md:h-[450px] md:min-h-80 md:p-8 md:text-4xl md:leading-[1.05] lg:text-5xl">
-									Presenting you with the best UI possible.
+								<article className="relative mx-auto h-[350px] min-h-60 max-w-[450px] overflow-hidden rounded-3xl border bg-gradient-to-b from-[#e60a64] to-[#e60a64]/5 p-6 text-3xl tracking-tight text-white md:h-[450px] capitalize md:min-h-80 md:p-8 md:text-4xl md:leading-[1.05] lg:text-5xl">
+									Ready to elevate your product Let’s connect
 									<div className="absolute -right-20 -bottom-20 z-10 mx-auto flex h-full w-full max-w-[300px] items-center justify-center transition-all duration-700 hover:scale-105 md:-right-28 md:-bottom-28 md:max-w-[550px]">
 										<Earth
 											scale={1.1}
